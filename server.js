44e026e55425e9dbc85281089c7a39aeeadbc8a1
mani13qwe <mani12345qwert@gmail.com>
2026-02-26 13:25:16 +0530

    basic server

---
+++ server.js
@@ -0,0 +1,81 @@
+const express = require("express")
+const mongoose=require("mongoose")
+const app=express()
+const cors = require("cors");
+app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname, "public")));
+mongoose.connect("mongodb://127.0.0.1:27017/onlineShop")
+.then(()=>console.log("mongoDb is connected successfully"))
+.catch(err=>console.error("error",err))
+
+const schema=new mongoose.Schema({
+    itemId:{
+        type:Number,
+        unique:true,
+        required:true
+    },
+    itemName:String,
+    price:Number,
+    available:Number
+})
+
+const onlineShop=mongoose.model("shopping",schema)
+
+app.post("/api/items",async (req,res)=>{
+    try{
+        const online=await onlineShop.insertOne(req.body)
+        console.log("Item added successfully")
+        res.status(200).send("added successfully")
+    }
+    catch(err){
+        const error=err.message
+        res.status(500).send(error)
+    }
+
+})
+app.get("/api/items",async(req,res)=>{
+    try{
+        const data=await onlineShop.find()
+        res.send(data)
+    }catch(err){
+        res.status(500).send(err)
+    }
+})
+
+app.put("/api/items/:itemId", async (req,res)=>{
+    try{
+        const result=await onlineShop.updateOne(
+        { itemId:req.params.itemId},
+        {$set:req.body}
+    )
+     if (result.matchedCount === 0) {
+            return res.status(404).send("Item not found");
+        }
+    res.send("data updated")
+    }catch(err){
+        res.status(500).send(err)
+    }
+
+})
+
+app.delete("/api/items/:itemId", async (req,res)=>{
+    try{
+        const result=await onlineShop.deleteOne(
+        { itemId:req.params.itemId}
+    )
+       if (result.deletedCount === 0) {
+            return res.status(404).send("Item not found");
+        }
+    return res.send("data deleted")
+    }catch(err){
+        res.status(500).send(err)
+    }
+
+})
+
+
+app.listen(3000,()=>{
+    console.log("server is running at 3000 port")
+})
